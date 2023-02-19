4d8d35a9af0597cc6955545dc7fbee79a73613c4
박동규 <ssudomt@naver.com>
2023-02-19 19:52:19 +0900

    배열의 유사도
    +1점
    total:1067

---
+++ 공부/배열의 유사도.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+    for (string s : s1) {
+        if (find(s2.begin(), s2.end(), s)!=s2.end()) answer++;
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<string>a{ "a", "b", "c" };
+    vector<string>b{ "com", "b", "d", "p", "c" };
+    cout << solution(a, b);
+}
