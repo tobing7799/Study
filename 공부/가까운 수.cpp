eda78a73264a43b43f841ce14984fbeba687df92
박동규 <ssudomt@naver.com>
2023-02-26 16:28:54 +0000

    가까운 수
    +13점
    total:1116점

---
+++ 공부/가까운 수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    sort(array.begin(), array.end());
+    auto p = find_if(array.begin(), array.end(), [n](int a) {
+        if (n < a) return true;
+        else return false; });
+    if (*p - n < n - *(p - 1)) answer = *p;
+    else answer = *(p - 1);
+    return answer;
+}
+
+int main()
+{
+    vector<int> input{ 3, 10, 28 };
+    cout << solution(input, 20);
+}
