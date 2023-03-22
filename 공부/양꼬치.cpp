8343091b0264d32d77d3f832805d693393d24a79
박동규 <ssudomt@naver.com>
2023-03-22 20:23:43 +0900

    양꼬치
    +1점
    total:1285점

---
+++ 공부/양꼬치.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int k) {
+    int answer = 0;
+    return answer = (12000 * n) + ((k - (n / 10)) * 2000);
+}
+
+int main()
+{
+    cout << solution(10,3);
+}
