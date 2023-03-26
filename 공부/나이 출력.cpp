a7b50c50cddda334247ff65050b258259f882d30
박동규 <ssudomt@naver.com>
2023-03-27 02:12:44 +0900

    나이 출력
    +1점
    total:1293점

---
+++ 공부/나이 출력.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    return answer = 2023-age;
+}
+
+int main()
+{
+    cout << solution(40);
+}
