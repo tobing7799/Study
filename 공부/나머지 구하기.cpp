d32472649b252bfb482dadfd2381a4612f7fc261
박동규 <ssudomt@naver.com>
2023-03-31 11:47:23 +0900

    나머지 구하기
    +1점
    total:1307점

---
+++ 공부/나머지 구하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    return answer = num1 % num2;
+}
+
+int main()
+{
+    cout << solution(3, 2);
+}
