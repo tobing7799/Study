54489ddb29e5d7697613791407a32d4d47fadfca
박동규 <ssudomt@naver.com>
2023-04-01 20:55:58 +0900

    숫자 비교하기
    +1점
    total:1312점

---
+++ 공부/숫자 비교하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    return answer = num1==num2 ? 1: -1;
+}
+
+int main()
+{
+    cout << solution(2, 3);
+}
