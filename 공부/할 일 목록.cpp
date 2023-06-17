3ea038481b7c91a77663747783fb520197d274f6
박동규 <ssudomt@naver.com>
2023-06-18 03:24:04 +0900

    할 일 목록
    +1점
    total:1488점

---
+++ 공부/할 일 목록.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(vector<string> todo_list, vector<bool> finished) {
+    vector<string> answer;
+    for (string s : todo_list)
+    {
+        if (!finished.front()) answer.push_back(s);
+        finished.erase(finished.begin());
+    }
+    return answer;
+}
+
+int main()
+{
+    for (string s : solution({ "problemsolving", "practiceguitar", "swim", "studygraph" }, { true, false, true, false }))
+        cout << s << " ";
+    }
+}
