388300c9345062fd15475154f636991de173c119
박동규 <ssudomt@naver.com>
2023-07-01 07:43:07 +0900

    공백으로 구분하기 1
    +1점
    total:1505점

---
+++ 공부/공백으로 구분하기 1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    stringstream str(my_string);
+    string s;
+    while (str >> s)
+    {
+        answer.push_back(s);
+    }
+    return answer;
+}
+
+int main()
+{
+    for (string s : solution("i love you"))
+    {
+        cout << s << " ";
+    }
+}
