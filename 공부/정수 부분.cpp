27f4c655de2b215e2e78a24ff0928834e753bc7c
박동규 <ssudomt@naver.com>
2023-07-20 22:52:58 +0900

    정수 부분
    +1점
    total:1528점

---
+++ 공부/정수 부분.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(double flo) {
+    int answer = 0;
+    return answer = flo;
+}
+
+int main()
+{
+    cout << solution(1.42);
+}
