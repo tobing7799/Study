093288f56ae5c39f962110aa33f6bc0f5b2a5751
박동규 <ssudomt@naver.com>
2023-07-25 01:59:04 +0900

    문자열로 변환

---
+++ 공부/문자열로 변환.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    return answer = to_string(n);
+}
+
+int main()
+{
+    cout << solution(123);
+}
