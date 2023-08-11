85f2a07e0a1cbbc725662a7417cdaf5b6ae79f7e
박동규 <ssudomt@naver.com>
2023-08-11 16:28:23 +0900

    나머지가 1이 되는 수 찾기
    +1점
    total:1552점

---
+++ 공부/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (n % i == 1)
+        {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(10);
+}
