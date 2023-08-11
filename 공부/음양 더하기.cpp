871e94e7ea085df086accea4660628cdae68f3b8
tobing7799 <ssudomt@naver.com>
2023-08-12 00:26:12 +0900

    음양 더하기
    +1점
    total:1553점

---
+++ 공부/음양 더하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < absolutes.size(); ++i)
+    {
+        if (signs[i]) answer += absolutes[i];
+        else answer -= absolutes[i];
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ 4,7,12 },{ true,false,true });
+}
