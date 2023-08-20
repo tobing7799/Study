fa845ee16d60e717a33c46bbc3cb5daafff788a0
박동규 <ssudomt@naver.com>
2023-08-20 22:49:56 +0900

    문자열을 정수로 바꾸기
    +1점
    total:1561점

---
+++ 공부/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    return answer = stoi(s);
+}
+
+int main()
+{
+    cout << solution("1234");
+}
