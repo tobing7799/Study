4d24500a299bf308a25f0561d7131eb59e8e21ca
박동규 <ssudomt@naver.com>
2023-08-24 00:24:35 +0900

    수박수박수박수박수박수?
    +1점
    total:1567점

---
+++ 공부/수박수박수박수박수박수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for (int i = 0; i < n; ++i)
+    {
+        if (i % 2 == 0) answer += "¼ö";
+        else answer += "¹Ú";
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(3);
+}
