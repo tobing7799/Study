958085036ccf52447146874649ba1d0db1ff1116
박동규 <ssudomt@naver.com>
2023-09-19 22:26:25 +0900

    n^2 배열 자르기
    +2점
    total:1598점

---
+++ 공부/n^2 배열 자르기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    for (long long i = left; i <= right; i++) {
+        int r, c;
+        r = i / n + 1;
+        c = i % n + 1;
+        answer.push_back(max(r,c));
+
+    }
+    return answer;
+}
+
+int main()
+{
+    for (int n : solution(3,2,5)) {
+        cout << n << endl;
+    }
+}
