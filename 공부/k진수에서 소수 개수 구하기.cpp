2a9f72db47d4593b67300dc4f63a128491918174
박동규 <ssudomt@naver.com>
2023-09-23 22:24:27 +0900

    k진수에서 소수 개수 구하기
    +4점
    total:1610점

---
+++ 공부/k진수에서 소수 개수 구하기.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(long long n) {
+    if (n == 0 || n == 1) return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int solution(int n, int k) {
+    int answer = 0;
+    string num;
+    string result = "";
+    while (n != 0) {
+        num += to_string(n % k);
+        n /= k;
+    }
+    reverse(num.begin(), num.end());
+
+    for (auto c : num) {
+        if (c == '0') {
+            if (result.length() == 0) continue;
+            if (isPrime(stol(result)) == true) answer++;
+            result = "";
+        }
+        else result += c;
+    }
+    if (result.length() != 0 && isPrime(stol(result)) == true) answer++;
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(437674, 3);
+}
